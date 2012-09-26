2cb14e18c7417f5f0345be03be06ddae757dcf81
Daniel Siwiec <daniel.siwiec@gmail.com>
2012-09-25 19:21:25 -0500

    Rolling back repackaging

--- src/main/com/danielsiwiec/sort/Sorter.java
+++ src/main/com/danielsiwiec/sort/Sorter.java
@@ -2,8 +2,6 @@ package com.danielsiwiec.sort;
 
 import java.util.List;
 
-import com.danielsiwiec.sort.strategies.SortStrategy;
-
 public class Sorter {
 
 	public List<Integer> sort(List<Integer> integers, SortStrategy strategy) {
--- src/main/com/danielsiwiec/sort/strategies/SortStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.danielsiwiec.sort.strategies;
-
-import java.util.List;
-
-public interface SortStrategy {
-	
-	public List<Integer> sort(List<Integer> list);
-
-}
