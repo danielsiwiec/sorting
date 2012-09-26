27ffd4d5090563e7428c982287486fd83e6dd205
Daniel Siwiec <daniel.siwiec@gmail.com>
2012-09-25 19:14:07 -0500

    Converting to TestNG and implementing DataProvider

--- src/test/com/danielsiwiec/sort/SortTest.java
+++ src/test/com/danielsiwiec/sort/SortTest.java
@@ -1,21 +1,21 @@
 package com.danielsiwiec.sort;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.testng.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.Test;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
 public class SortTest {
 
-	private SortStrategy strategy = new MergeSort();
 	private Sorter sorter = new Sorter();
-
-	@Test
-	public void simpleTest() {
+	
+	@Test(dataProvider="strategies")
+	public void simpleTest(SortStrategy strategy) {
 		Integer[] array = {3,2,1};
 		List<Integer> sorted = sorter.sort(Arrays.asList(array), strategy);
 		assertNotNull(sorted);
@@ -25,21 +25,21 @@ public class SortTest {
 		assertEquals(3,(int)sorted.get(2));
 	}
 	
-	@Test
-	public void acceptsNullList(){
+	@Test(dataProvider="strategies")
+	public void acceptsNullList(SortStrategy strategy){
 		List<Integer> sorted = sorter.sort(null, strategy);
 		assertNull(sorted);
 	}
 	
-	@Test
-	public void acceptsEmptyList(){
+	@Test(dataProvider="strategies")
+	public void acceptsEmptyList(SortStrategy strategy){
 		List<Integer> sorted = sorter.sort(new ArrayList<Integer>(), strategy);
 		assertNotNull(sorted);
 		assertEquals(0,sorted.size());
 	}
 	
-	@Test
-	public void sorts(){
+	@Test(dataProvider="strategies")
+	public void sorts(SortStrategy strategy){
 		Integer[] array = {2,1,9,3,4,8,0,5,10,6};
 		Sorter sorter = new Sorter();
 		List<Integer> sorted = sorter.sort(Arrays.asList(array), strategy);
@@ -55,6 +55,14 @@ public class SortTest {
 		assertEquals(8,(int)sorted.get(7));
 		assertEquals(9,(int)sorted.get(8));
 		assertEquals(10,(int)sorted.get(9));
+	}
+	
+	@DataProvider(name="strategies")
+	public Object[][] strategyProvider(){
+		return new Object[][]{
+				{new MergeSort()},
+				{new QuickSort()}
+		};
 	}
 
 }
